c5cd88d72d047dc612eeafc8ccce53d5b10a07bf
Marat Enaliev <enamarat@gmail.com>
2022-05-09 17:49:14 +0300

    Add an option to type special characters in Finnish alphabet.

--- src/components/section.js
+++ src/components/section.js
@@ -3,16 +3,31 @@ import React, { Component } from 'react';
 class Section extends Component {
     constructor(props) {
         super(props);
-       // this.state = {greetings: greetings};
+        this.state = {
+            userInput: ""
+        };
     }
 
-  
+
+    enterSpecialCharacter = (event) => {
+        this.setState({userInput: this.state.userInput + event.target.textContent});
+    }
+
+    handleChange = (event) => {
+        this.setState({userInput: event.target.value});
+    }
+
+
     render() {
      return(
         <div className='verticalContainer'>
             <h2 className="sectionTitle">{this.props.currentSection.toUpperCase()}</h2>
             <p>{this.props.tasks[0].english}</p>
-            <textarea className="userInput"></textarea>
+            <textarea value={this.state.userInput} className="userInput" onChange={this.handleChange}></textarea>
+            <div className="specialCharsContainer">
+                <button onClick={this.enterSpecialCharacter} className="specialCharButton">ä</button>
+                <button onClick={this.enterSpecialCharacter} className="specialCharButton">ö</button>
+            </div>
             <button onClick={this.showSection}>Check</button>
             <button onClick={this.props.returnToSections}>Return</button>
         </div>
