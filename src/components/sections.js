aa3cd82f1c168538a62f9a7ac31af7a0138f0ce4
Marat Enaliev <enamarat@gmail.com>
2022-06-05 22:28:33 +0300

    Add a new section.

--- src/components/sections.js
+++ src/components/sections.js
@@ -5,26 +5,37 @@ import colors from '../tasks/colors.js';
 import persons from '../tasks/persons.js';
 import animals from '../tasks/animals.js';
 import countries from '../tasks/countries.js';
+import possession from '../tasks/possession.js';
 
 
 class Sections extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            sections: [
+                {name: 'greetings'},
+                {name: 'colors'},
+                {name: 'persons'},
+                {name: 'animals'},
+                {name: 'countries'},
+                {name: 'possession'}
+            ],
             greetings: greetings,
             colors: colors,
             persons: persons,
             animals: animals,
             countries: countries,
+            possession: possession,
             currentSection: null
         };
     }
 
+ 
     returnToSections = () => {
         this.setState({currentSection: null});
     }
 
-
+   
     showSection = (event) => {
         const section = event.target.textContent.toLowerCase();
         this.setState({currentSection: section});
@@ -33,13 +44,10 @@ class Sections extends Component {
 
     render() {
         if (this.state.currentSection == null) {
+            const content = this.state.sections.map((d,i) => <button key={i} onClick={this.showSection}>{d.name}</button>);
             return(
                 <div className='verticalContainer'>
-                    <button onClick={this.showSection}>Greetings</button>
-                    <button onClick={this.showSection}>Colors</button>
-                    <button onClick={this.showSection}>Persons</button>
-                    <button onClick={this.showSection}>Animals</button>
-                    <button onClick={this.showSection}>Countries</button>
+                    {content}
                 </div>
              );
         } else {
---
+++ src/tasks/possession.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        english: "I have a knife.",
+        finnish: "Minulla on veitsi."
+    },
+    {
+        english: "We have a plate.",
+        finnish: "Meillä on lautanen."
+    },
+    {
+        english: "You have a spoon.",
+        finnish: "Sinulla on lusikka."
+    },
+    {
+        english: "You have a glass.",
+        finnish: "Teillä on lasi."
+    },
+    {
+        english: "She has a fork.",
+        finnish: "Hänellä on haarukka."
+    },
+    {
+        english: "They have a cup.",
+        finnish: "Heillä on kuppi."
+    }
+];
